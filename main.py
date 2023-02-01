ba92393aaef9af55087ed79b6afc215aede93397
PolinaPozhydaieva <Ppolinka@icloud.com>
2023-02-01 14:00:44 +0200

    new message

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 # print("Happy new Year")
 
-a = int(input("wtite 1 num"))
-b = int(input("write 2 num"))
-c = input("write sign")
+# a = int(input("wtite 1 num"))
+# b = int(input("write 2 num"))
+# c = input("write sign")
 
-def calc( num1 , num2 , calcu):
-  if calcu == "+":
-    print(num1+ num2)
-  elif calcu == "-":
-    print(num1 - num2)
-  elif calcu == "*":
-    print(num1*num2)
-  elif calcu == "/":
-    print(num1/num2)
-  elif calcu == "*":
-    print(num1**num2)
-  else:
-    print("А давай ще раз , мені не сподобалось!")
-a = int(input("введіть число "))
-b = int(input("введіть число "))
-c = input("введіть дію ")
-calc(a , b , c)
+# def calc( num1 , num2 , calcu):
+#   if calcu == "+":
+#     print(num1+ num2)
+#   elif calcu == "-":
+#     print(num1 - num2)
+#   elif calcu == "*":
+#     print(num1*num2)
+#   elif calcu == "/":
+#     print(num1/num2)
+#   elif calcu == "*":
+#     print(num1**num2)
+#   else:
+#     print("А давай ще раз , мені не сподобалось!")
+# a = int(input("введіть число "))
+# b = int(input("введіть число "))
+# c = input("введіть дію ")
+# calc(a , b , c)
+
+#
+# a = int(input("wtite min num"))
+# b = int(input("write max num"))
+# for a in range(a,b+1):
+#     print(a)
+a = int(input("wtite min num"))
+b = int(input("write max num"))
+for a in range(b, a):
+    print(b-1)
