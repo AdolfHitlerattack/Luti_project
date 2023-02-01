22b495fc33b90aee3eb565051b1d58ce238fc8d0
PolinaPozhydaieva <Ppolinka@icloud.com>
2023-02-01 13:45:35 +0200

    new message

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Happy new Year")
+# print("Happy new Year")
+
+a = int(input("wtite 1 num"))
+b = int(input("write 2 num"))
+c = input("write sign")
+
+def calc( num1 , num2 , calcu):
+  if calcu == "+":
+    print(num1+ num2)
+  elif calcu == "-":
+    print(num1 - num2)
+  elif calcu == "*":
+    print(num1*num2)
+  elif calcu == "/":
+    print(num1/num2)
+  elif calcu == "*":
+    print(num1**num2)
+  else:
+    print("А давай ще раз , мені не сподобалось!")
+a = int(input("введіть число "))
+b = int(input("введіть число "))
+c = input("введіть дію ")
+calc(a , b , c)
